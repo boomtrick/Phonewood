ac8ebba7e75a0c71084f217de696dbe7c84a34be
boomtrick <markjitthompson@gmail.com>
2016-05-01 20:54:55 -0400

    updated project with right names, added Call History button

---
+++ Phonewood/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Phoneword;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phone word", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            callButton.Enabled = false;
+
+            string translatedNumber = string.Empty;
+
+            //when user presses translate button translate string to number and enable calling function.
+            translateButton.Click += (object sender, EventArgs e) =>
+            { 
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                //create alert dialog that asks if user really wants to call
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate{
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel: " + translatedNumber));
+                    StartActivity(callIntent);
+                });
+
+                callDialog.SetNegativeButton("Cancel", delegate { });
+                callDialog.Show();
+            };
+        }
+    }
+}
+
